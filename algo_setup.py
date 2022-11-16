a724bb71fc3dbc3b2565d409a18e204bca6814d3
ozzs <ozz.shaf@gmail.com>
2022-11-16 15:18:44 +0200

    Re-organized code and added comments

--- algo_setup.py
+++ algo_setup.py
@@ -1,25 +1,5 @@
 import random
 
-def mh_problem_human_p1(choice_index):
-    l1 = ["goat", "car", "goat"]
-    random.shuffle(l1)
-    print(l1)
-    l2 = [0, 1, 2]
-    ci = l1.index("car")
-    l3 = []
-    for i in range(len(l1)):
-        if i != ci:
-            l3.append(i)
-    s = ""
-    for x in l3:
-      s = s + " " + str(x)
-      if x != choice_index:
-          k = x
-    for i in range(len(l2)):
-        if l2[i] != choice_index and l2[i] != k:
-            sc = i
-            break
-    return k, l1
 
 
 def mh_problem(should_print, text_box):
@@ -62,14 +42,34 @@ def mh_problem(should_print, text_box):
     return ci, fc, sc
 
 
+def mh_problem_partial(choice_index):
+    obj_list = ["goat", "car", "goat"]
+    random.shuffle(obj_list)
+    print(obj_list)
+    l2 = [0, 1, 2]
+    ci = obj_list.index("car")
+    l3 = []
+    for i in range(len(obj_list)):
+        if i != ci:
+            l3.append(i)
+    s = ""
+    for x in l3:
+      s = s + " " + str(x)
+      if x != choice_index:
+          exposed_goat_index = x
+    for i in range(len(l2)):
+        if l2[i] != choice_index and l2[i] != exposed_goat_index:
+            sc = i
+            break
+    return exposed_goat_index, obj_list
+
+
 def game_run(n, should_print, text_box):
     wins = 0
     losses = 0
     for i in range(n):
         if should_print:
             print("\ngame number: " + str(i + 1))
-            # my_text = text_box["text"] + "\ngame number: " + str(i + 1)
-            # text_box.config(text=my_text)
         ci, fc, sc = mh_problem(should_print, text_box)
         if ci == sc:
             wins += 1
--- main.py
+++ main.py
@@ -1,73 +1,66 @@
 from tkinter import *
-from tkinter import ttk
 from PIL import ImageTk, Image
-from AlgoSetup import game_run, mh_problem_human_p1
+from algo_setup import game_run, mh_problem_partial
 import pygame
 import random
-# import os
 
 
-# def restart():
-#     root.destroy()
-#     os.startfile("main.py")
-
-
-def create_new_window(frame_destroy):
-    frame_destroy.destroy()
+def create_new_window(frame_destroy=None):
+    # Destroy previous frame
+    if frame_destroy != None:
+        frame_destroy.destroy()
     new_frame = Frame(width=650, height=500)
-    new_frame.pack(fill="both", expand=True, padx=0, pady=0)
-    new_frame.columnconfigure(0, weight=1)
-    new_frame.columnconfigure(1, weight=1)
-    new_frame.columnconfigure(2, weight=1)
-    Label(new_frame, image=bg).place(x=0, y=0)
+    new_frame.pack(fill="both", expand=True)
+
+    # Setting the padding between columns
+    for i in range(3):
+        new_frame.columnconfigure(i, weight=10)
+
+    # Place the background image
+    Label(new_frame, image=background_img).place(x=0, y=0)
     return new_frame
 
 
-def human_run():
+def human_run(main_frame):
+    # Create new frame for 'Human Run'
+    first_human_frame = create_new_window(main_frame)
 
-    first_human_frame = create_new_window(frame)
+    Label(first_human_frame, text="Make your choice").grid(column=1, row=0, pady=10)
 
-    ttk.Label(first_human_frame, text="Make your choice").grid(column=1, row=0, pady=10)
-    Label(first_human_frame, image=curtains_img_tk).grid(column=0, row=1)
-    Label(first_human_frame, image=curtains_img_tk).grid(column=1, row=1)
-    Label(first_human_frame, image=curtains_img_tk).grid(column=2, row=1)
+    # Render 3 curtain images & 3 buttons to choose from
+    for i in range(3):
+        Label(first_human_frame, image=curtains_img).grid(column=i, row=1)
+        Button(
+            first_human_frame,
+            text="No." + str(i + 1),
+            command=lambda: human_sec_choice(i, first_human_frame),
+        ).grid(column=i, row=2, pady=10)
 
-    Button(
-        first_human_frame,
-        text="NO.1",
-        command=lambda: human_sec_choice(0, first_human_frame),
-    ).grid(column=0, row=2, pady=10)
-    Button(
-        first_human_frame,
-        text="NO.2",
-        command=lambda: human_sec_choice(1, first_human_frame),
-    ).grid(column=1, row=2, pady=10)
-    Button(
-        first_human_frame,
-        text="NO.3",
-        command=lambda: human_sec_choice(2, first_human_frame),
-    ).grid(column=2, row=2, pady=10)
     root.mainloop()
 
 
 def human_sec_choice(first_choice, first_frame):
-    exposed_goat_index, l1 = mh_problem_human_p1(first_choice)
+    # Create new frame for screen after 1st choice
     second_human_frame = create_new_window(first_frame)
-    ttk.Label(second_human_frame, text="Now do you want to change your choice?").grid(
+
+    # Run the Monty Hall algorithm -> return the exposed goat index & list
+    exposed_goat_index, obj_list = mh_problem_partial(first_choice)
+
+    Label(second_human_frame, text="Would you like to change your choice?").grid(
         column=1, row=0, pady=10
     )
 
     for i in range(3):
         if i == exposed_goat_index:
-            image_label = Label(second_human_frame, image=goat_img_tk)
+            image_label = Label(second_human_frame, image=goat_img)
         else:
-            image_label = Label(second_human_frame, image=curtains_img_tk)
+            image_label = Label(second_human_frame, image=curtains_img)
             if i == 0:
                 Button(
                     second_human_frame,
                     text="Change" if i != first_choice else "Stand",
                     command=lambda: human_results(
-                        first_choice, 0, l1, second_human_frame
+                        first_choice, 0, obj_list, second_human_frame
                     ),
                 ).grid(column=i, row=2)
             elif i == 1:
@@ -75,7 +68,7 @@ def human_sec_choice(first_choice, first_frame):
                     second_human_frame,
                     text="Change" if i != first_choice else "Stand",
                     command=lambda: human_results(
-                        first_choice, 1, l1, second_human_frame
+                        first_choice, 1, obj_list, second_human_frame
                     ),
                 ).grid(column=i, row=2)
             else:
@@ -83,194 +76,256 @@ def human_sec_choice(first_choice, first_frame):
                     second_human_frame,
                     text="Change" if i != first_choice else "Stand",
                     command=lambda: human_results(
-                        first_choice, 2, l1, second_human_frame
+                        first_choice, 2, obj_list, second_human_frame
                     ),
                 ).grid(column=i, row=2)
         image_label.grid(column=i, row=1)
     root.mainloop()
 
 
-def human_results(first_choice, second_choice, l1, second_human_frame):
+def human_results(first_choice, second_choice, obj_list, second_human_frame):
+    # Create new frame for displaying the results
     human_results_frame = create_new_window(second_human_frame)
-    ci = l1.index("car")
-    ttk.Label(
+
+    ci = obj_list.index("car")
+
+    # Render texts conditionally, depends on Win / Lose
+    Label(
         human_results_frame, text="You Won!" if ci == second_choice else "You Lost!"
     ).place(relx=0.5, rely=0.2, anchor=CENTER)
-    ttk.Label(
+    Label(
         human_results_frame,
         text="Choice changed"
         if first_choice != second_choice
         else "Choice not changed",
     ).place(relx=0.5, rely=0.25, anchor=CENTER)
 
+    # Render image conditionally, depends on Win / Lose
     image_label = Label(
-        human_results_frame, image=car_image_tk if ci == second_choice else goat_img_tk
+        human_results_frame, image=car_img if ci == second_choice else goat_img
     )
     image_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
     if ci == second_choice:
-        play("Sounds/ApplauseSound.wav")
+        play_sound("sounds/ApplauseSound.wav")
     else:
-        play("Sounds/GoatSound.mp3")
+        play_sound("sounds/GoatSound.mp3")
 
-    # Button(
-    #     human_results_frame,
-    #     text="Restart",
-    #     command=lambda: restart()
-    # ).place(relx=0.5, rely=0.7, anchor=CENTER)
+    # Restart button
+    Button(
+        human_results_frame,
+        text="Restart",
+        command=lambda: main_screen(human_results_frame),
+    ).place(relx=0.5, rely=0.7, anchor=CENTER)
 
     root.mainloop()
 
 
-def pc_multirun():
-    pc_multirun_frame = create_new_window(frame)
-    ttk.Label(pc_multirun_frame, text="Times to run:").grid(column=1, row=6, pady=10)
-    times_to_run = [("100", 100), ("1000", 1000), ("10,000", 10000), ("100,000", 100000)]
+def pc_multirun(main_frame):
+    # Create new frame for 'PC Multi-Run'
+    pc_multirun_frame = create_new_window(main_frame)
+
+    Label(pc_multirun_frame, text="Times to run:").grid(column=1, row=0, pady=10)
+
+    # Times to run radio buttons
+    times_to_run = [
+        ("100", 100),
+        ("1000", 1000),
+        ("10,000", 10000),
+        ("100,000", 100000),
+    ]
     v = IntVar()
     v.set(100)
-    ttk.Button(pc_multirun_frame, text="Select", command=lambda: pc_multirun_res(pc_multirun_frame, v.get())).grid(column=1, row=1, pady=10)
     i = 0
     for times, val in times_to_run:
-        b = ttk.Radiobutton(pc_multirun_frame, text=times, variable=v, command=v.get(), value=val)
-        b.grid(column=1, row=7 + i)
+        b = Radiobutton(
+            pc_multirun_frame, text=times, variable=v, command=v.get(), value=val
+        )
+        b.grid(column=1, row=1 + i)
         i += 1
+
+    # Times to run select button
+    Button(
+        pc_multirun_frame,
+        text="Select",
+        command=lambda: pc_multirun_res(pc_multirun_frame, v.get()),
+    ).grid(column=1, row=6, pady=10)
 
 
 def pc_multirun_res(pc_multirun_frame, n):
+    # Create new frame for displaying the results
     pc_multirun_frame_res = create_new_window(pc_multirun_frame)
-    text_box = ttk.Label(pc_multirun_frame_res, width=100, text="")
+
+    # Run the Monty Hall algorithm -> returns Win (True) / Lose (False)
+    text_box = Label(pc_multirun_frame_res, width=100, text="")
     text_box.grid(column=0, row=0)
     wins = game_run(n, False, text_box)
-    play("Sounds/ApplauseSound.wav"if wins else "Sounds/GoatSound.mp3")
-    image_label = Label(
-        pc_multirun_frame_res, image=car_image_tk if wins else goat_img_tk
+
+    # Play sound conditionally, depends on Win / Lose
+    play_sound("sounds/ApplauseSound.wav" if wins else "sounds/GoatSound.mp3")
+
+    # Render image conditionally, depends on Win / Lose
+    Label(pc_multirun_frame_res, image=car_img if wins else goat_img).place(
+        relx=0.5, rely=0.5, anchor=CENTER
     )
-    image_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
-    ttk.Label(
-            pc_multirun_frame_res, text="You Won!" if wins else "You Lost!"
-        ).place(relx=0.5, rely=0.2, anchor=CENTER)
-    ttk.Label(
+    # Render texts conditionally, depends on Win / Lose
+    Label(pc_multirun_frame_res, text="You Won!" if wins else "You Lost!").place(
+        relx=0.5, rely=0.2, anchor=CENTER
+    )
+    Label(pc_multirun_frame_res, text="Choice changed").place(
+        relx=0.5, rely=0.25, anchor=CENTER
+    )
+
+    # Restart button
+    Button(
         pc_multirun_frame_res,
-        text="Choice changed"
-    ).place(relx=0.5, rely=0.25, anchor=CENTER)
+        text="Restart",
+        command=lambda: main_screen(pc_multirun_frame_res),
+    ).place(relx=0.5, rely=0.7, anchor=CENTER)
 
 
-def pc_single_run():
-    pc_single_frame = create_new_window(frame)
+def pc_single_run(main_frame):
+    # Create new frame for 'PC Single Run'
+    pc_single_frame = create_new_window(main_frame)
+
+    # Randomize a choice
     choice = random.randint(0, 2)
-    text_box = ttk.Label(pc_single_frame, text="\nThe PC choice was No." + str(choice+1))
-    text_box.grid(column=1, row=0, pady=10)
-    Label(pc_single_frame, image=curtains_img_tk).grid(column=0, row=1)
-    Label(pc_single_frame, image=curtains_img_tk).grid(column=1, row=1)
-    Label(pc_single_frame, image=curtains_img_tk).grid(column=2, row=1)
+    Label(pc_single_frame, text="\nThe PC choice was No." + str(choice + 1)).grid(
+        column=1, row=0, pady=10
+    )
+
+    # Render 3 curtain images
+    for i in range(3):
+        Label(pc_single_frame, image=curtains_img).grid(column=i, row=1)
+
+    # Next step button
     Button(
         pc_single_frame,
         text="Next Step",
-        command=lambda: pc_single_run_p2(
-            pc_single_frame, choice
-        ),
+        command=lambda: pc_single_run_p2(pc_single_frame, choice),
     ).grid(column=1, row=2, pady=10)
 
 
 def pc_single_run_p2(pc_single_frame, choice):
+    # Create new frame for next step
     pc_single_p2_frame = create_new_window(pc_single_frame)
-    exposed_goat, l1 = mh_problem_human_p1(choice)
-    text_box = ttk.Label(pc_single_p2_frame,
-                         text="\nThe PC first choice was No." + str(choice + 1)+"\nThe game show expose curtain NO."+str(exposed_goat+1))
+
+    # Run the Monty Hall algorithm -> return the exposed goat index & list
+    exposed_goat_index, obj_list = mh_problem_partial(choice)
+    text_box = Label(
+        pc_single_p2_frame,
+        text="\nThe PC first choice was No."
+        + str(choice + 1)
+        + "\nThe game show expose curtain NO."
+        + str(exposed_goat_index + 1),
+    )
     text_box.grid(column=1, row=0, pady=10)
-    Label(pc_single_p2_frame, image=goat_img_tk).grid(column=exposed_goat, row=1)
+    Label(pc_single_p2_frame, image=goat_img).grid(column=exposed_goat_index, row=1)
     for i in range(3):
-        if i != exposed_goat:
-            Label(pc_single_p2_frame, image=curtains_img_tk).grid(column=i, row=1)
+        if i != exposed_goat_index:
+            Label(pc_single_p2_frame, image=curtains_img).grid(column=i, row=1)
 
     print("old choice: " + str(choice))
-    print("expose goat: " + str(exposed_goat))
-    ci = l1.index("car")
+    print("expose goat: " + str(exposed_goat_index))
+    ci = obj_list.index("car")
     arr = [0, 1, 2]
     arr.remove(choice)
-    arr.remove(exposed_goat)
+    arr.remove(exposed_goat_index)
     print("new choice: " + str(arr[0]))
     Button(
         pc_single_p2_frame,
         text="Next Step",
-        command=lambda: pc_single_run_res(
-            pc_single_p2_frame, arr[0], ci
-        ),
+        command=lambda: pc_single_run_res(pc_single_p2_frame, arr[0], ci),
     ).grid(column=1, row=2, pady=10)
 
 
 def pc_single_run_res(pc_single_p2_frame, new_choice, ci):
     pc_single_res_frame = create_new_window(pc_single_p2_frame)
 
-    text_box = ttk.Label(pc_single_res_frame,
-                         text="\nThe PC changed choice to No." + str(new_choice+1))
+    text_box = Label(
+        pc_single_res_frame, text="\nThe PC changed choice to No." + str(new_choice + 1)
+    )
     text_box.place(relx=0.5, rely=0.2, anchor=CENTER)
 
     if ci == new_choice:
-        Label(pc_single_res_frame, image=car_image_tk).place(relx=0.5, rely=0.5, anchor=CENTER)
-        play("Sounds/ApplauseSound.wav")
-        text_box_res = ttk.Label(pc_single_res_frame, text="PC Won!!!")
+        Label(pc_single_res_frame, image=car_img).place(
+            relx=0.5, rely=0.5, anchor=CENTER
+        )
+        play_sound("sounds/ApplauseSound.wav")
+        text_box_res = Label(pc_single_res_frame, text="PC Won!!!")
 
     else:
-        Label(pc_single_res_frame, image=goat_img_tk).place(relx=0.5, rely=0.5, anchor=CENTER)
-        play("Sounds/GoatSound.mp3")
-        text_box_res = ttk.Label(pc_single_res_frame, text="PC Lost...")
+        Label(pc_single_res_frame, image=goat_img).place(
+            relx=0.5, rely=0.5, anchor=CENTER
+        )
+        play_sound("sounds/GoatSound.mp3")
+        text_box_res = Label(pc_single_res_frame, text="PC Lost...")
     text_box_res.place(relx=0.5, rely=0.3, anchor=CENTER)
 
+    Button(
+        pc_single_res_frame,
+        text="Restart",
+        command=lambda: main_screen(pc_single_res_frame),
+    ).place(relx=0.5, rely=0.7, anchor=CENTER)
 
-def play(sound):
-    pygame.mixer.music.load(sound)
+
+# Function to play different sounds
+def play_sound(sound_path: str):
+    pygame.mixer.music.load(sound_path)
     pygame.mixer.music.play(loops=0)
 
 
+# Initiate
 root = Tk()
-root.title("Seminar")
+root.title("Monty Hall Problem")
 root.geometry("650x500")
 root.resizable(False, False)
 pygame.mixer.init()
 
-frame = Frame(root, width=650, height=500, background="#D3D3D3")
-frame.pack(fill="both", expand=True, padx=0, pady=0)
+# Function to load image (image: name, width, height)
+def load_image(image_name: str, width: int, height: int) -> PhotoImage:
+    image = Image.open(image_name).resize((width, height))
+    image_tk = ImageTk.PhotoImage(image)
+    return image_tk
 
-bg_img = Image.open("bg_img.jpg")
-bg_img = bg_img.resize((650, 500))
-bg = ImageTk.PhotoImage(bg_img)
 
-bg_label = Label(frame, image=bg)
-bg_label.place(x=0, y=0)
-frame.columnconfigure(0, weight=1)
-frame.columnconfigure(1, weight=1)
-frame.columnconfigure(2, weight=1)
+# Load images
+curtains_img = load_image("images/curtains_image.webp", 200, 150)  # Curtains Image
+goat_img = load_image("images/goat_image.jpg", 200, 150)  # Goat Image
+car_img = load_image("images/car_image.jpg", 200, 150)  # Car Image
+background_img = load_image("images/bg_img.jpg", 650, 500)  # Background Image
 
-ttk.Label(frame, text="Welcome To The Monty Hall Game").grid(column=1, row=0, pady=10)
+# Function to load main (initial) screen
+def main_screen(frame=None):
+    main_frame = create_new_window(frame)
 
-curtains_img = Image.open("curtains_image.webp")
-curtains_img = curtains_img.resize((200, 150))
-curtains_img_tk = ImageTk.PhotoImage(curtains_img)
-curtains_label_1 = Label(frame, image=curtains_img_tk)
-curtains_label_1.grid(column=0, row=1)
-curtains_label_2 = Label(frame, image=curtains_img_tk)
-curtains_label_2.grid(column=1, row=1)
-curtains_label_3 = Label(frame, image=curtains_img_tk)
-curtains_label_3.grid(column=2, row=1)
+    # Title
+    Label(main_frame, text="Welcome To The Monty Hall Game").grid(
+        column=1, row=0, pady=10
+    )
 
-goat_img = Image.open("goat_image.jpg")
-goat_img = goat_img.resize((200, 150))
-goat_img_tk = ImageTk.PhotoImage(goat_img)
+    # Render 3 curtain images to screen
+    for i in range(3):
+        Label(main_frame, image=curtains_img).grid(column=i, row=1)
 
-car_image = Image.open("car_image.jpg")
-car_image = car_image.resize((200, 150))
-car_image_tk = ImageTk.PhotoImage(car_image)
+    Label(main_frame, text="Choose Game Type:").grid(column=1, row=3, pady=10)
 
-ttk.Label(frame, text="Choose Game Type:").grid(column=1, row=3, pady=10)
+    # PC Single-Run button
+    Button(
+        main_frame, text="PC Single-Run", command=lambda: pc_single_run(main_frame)
+    ).grid(column=0, row=4, pady=10)
+    # PC Multi-Run button
+    Button(main_frame, text="Statistics", command=lambda: pc_multirun(main_frame)).grid(
+        column=1, row=4, pady=10
+    )
+    # Human Run button
+    Button(main_frame, text="Human Run", command=lambda: human_run(main_frame)).grid(
+        column=2, row=4, pady=10
+    )
 
-ttk.Button(frame, text="PC Single-Run", command=pc_single_run).grid(
-    column=0, row=4, pady=10
-)
-ttk.Button(frame, text="Statistics", command=pc_multirun).grid(
-    column=1, row=4, pady=10
-)
-ttk.Button(frame, text="Human Run", command=human_run).grid(column=2, row=4, pady=10)
+    root.mainloop()
 
-root.mainloop()
+
+if __name__ == "__main__":
+    main_screen()
